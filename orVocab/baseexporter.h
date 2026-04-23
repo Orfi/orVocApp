7548c8917aee098bb266b087ec28464ec1a4326c
Orfi <waelorfi@aucegypt.edu>
2026-04-23 08:32:33 +0200

    feat(RVC-31): ADDED: BaseExporter abstract class header with WordEntry struct

---
+++ orVocab/baseexporter.h
@@ -0,0 +1,49 @@
+#ifndef BASEEXPORTER_H
+#define BASEEXPORTER_H
+
+#include <QObject>
+#include <QString>
+#include <QUrl>
+#include <QVector>
+
+class QNetworkAccessManager;
+class QThread;
+
+struct WordEntry {
+    QString word;
+    QString phonetic;
+    QString definitionHtml;
+    QString translationHtml;
+    bool valid = false;
+};
+
+class BaseExporter : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit BaseExporter(const QStringList &words, QObject *parent = nullptr);
+    ~BaseExporter() override;
+
+    void exportToFile(const QString &outputPath);
+
+signals:
+    void progress(int current, int total);
+    void finished(bool success, const QString &filePath);
+
+protected:
+    virtual bool renderToFile(const QVector<WordEntry> &entries, const QString &outputPath) = 0;
+
+private slots:
+    void doWork();
+
+private:
+    void fetchWord(const QString &word, WordEntry &entry);
+
+    QStringList m_words;
+    QString m_outputPath;
+    QThread *m_thread = nullptr;
+    QNetworkAccessManager *m_nam = nullptr;
+};
+
+#endif // BASEEXPORTER_H
